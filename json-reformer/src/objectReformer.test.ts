c97303fc2046bf8f09f97b4fafa60e900426d0fe
Babak Ansari <babakansari@hotmail.com>
2025-01-21 22:31:27 -0500

    Add more unit test cases

--- json-reformer/src/objectReformer.test.ts
+++ json-reformer/src/objectReformer.test.ts
@@ -21,17 +21,25 @@ describe('test objectReformer', () => {
       prop1: {
         prop10: 1,
         prop11: 11,
+        prop12: {
+          prop120: 120,
+          prop121: 121,
+          prop122: [
+            { prop1220: 1220 },
+            { prop1221: 1221 },
+            { prop1222: 1222 } 
+          ]
+        }
       } };
-    const reformerModel = { reformers: [{ "prop0": 100 }, { "prop1.prop11": 1100 }] };
+    const reformerModel = { reformers: [{ "prop0": 100 }, { "prop1.prop11": 1100 }, { "prop1.prop12.prop122[1]": { propWXYZ : 1234 } }] };
     const reformer = ObjectReformer(reformerModel);
     
-
     // Act
     const result = reformer.reform(input);
 
     // Assert
     expect(result.prop0).toEqual(100);
     expect(result.prop1.prop11).toEqual(1100);
-
+    expect(result.prop1.prop12.prop122[1].propWXYZ).toEqual(1234);
   });
 });
