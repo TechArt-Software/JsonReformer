85d786a4de21c7e7d093a813fff421f295bd4e37
Babak Ansari <babakansari@hotmail.com>
2025-02-06 16:55:17 -0500

    Add test to evaluate sum

--- json-reformer/src/ObjectReformer.test.ts
+++ json-reformer/src/ObjectReformer.test.ts
@@ -127,7 +127,6 @@ describe('test objectReformer', () => {
     expect(result.prop1.prop11.prop111[0].prop1111).toEqual(2222);
   });
 
-
   it('reform loop on all ReformerModel elemets and evaluate sum of the currentValue and newValue', () => {
     // Arrange
     const input = { 
@@ -159,5 +158,44 @@ describe('test objectReformer', () => {
 
     // Assert
     expect(result.prop1.prop11.prop111[0].prop1111).toEqual(3333);
+  });
+
+  it('reform loop on all ReformerModel elemets and evaluate sum of values in an array', () => {
+    // Arrange
+    const input = {
+      "array": [
+        {
+          "one": 1
+        },
+        {
+          "two": 2
+        },
+        {
+          "three": 3
+        }
+      ],
+      "Sum": 0
+    };
+    const reformerModel = {
+            reformers: [
+              {
+                Sum: 0
+              }
+            ],
+            scripts: [
+              {
+                action: "setProperty",
+                parameters: "input, property, currentValue, newValue",
+                body: "return input.array.reduce((acc, obj) => acc + Object.values(obj)[0], 0)"
+              }
+            ]
+          };
+    const reformer = ObjectReformer(reformerModel);
+    
+    // Act
+    const result = reformer.reform(input);
+
+    // Assert
+    expect(result.Sum).toEqual(6);
   });
 });
